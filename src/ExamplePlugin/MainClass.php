091884dd8464033d2994d9ec95821af904005aba
Keith @ Killa Network <keithkfng@gmail.com>
2015-10-28 18:35:59 +1100

    Update MainClass.php

--- src/ExamplePlugin/MainClass.php
+++ src/ExamplePlugin/MainClass.php
@@ -1,49 +1,40 @@
 <?php
 
-namespace ExamplePlugin;
+namespace ImagicalCorp\JoinBroadcast;
 
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
-use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerRespawnEvent;
-use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\Player;
 use pocketmine\Server;
-use pocketmine\utils\TextFormat;
+use pocketmine\event\Listener;
 
-class MainClass extends PluginBase implements Listener{
-
-	public function onLoad(){
-		$this->getLogger()->info(TextFormat::WHITE . "I've been loaded!");
-	}
-
-	public function onEnable(){
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->getServer()->getScheduler()->scheduleRepeatingTask(new BroadcastPluginTask($this), 120);
-		$this->getLogger()->info(TextFormat::DARK_GREEN . "I've been enabled!");
+class StaffJoin extends PluginBase implements Listener{
+   
+    public function onEnable() {
+       
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        
+        $this->getLogger()->info("Everything loaded!");
     }
+       
+       
+    }
+   
+    function onJoin(PlayerJoinEvent $event) {
+       
+        $player = $event->getPlayer();
+        $name = $player->getDisplayName();
+       
+        if ($name == "K3ith") {
+           
+            Server::getInstance()->broadcastMessage("The owner(K3ith) joined the game.");
+           
+        }
+       
+        else {
+                    Server::getInstance()->broadcastMessage("Welcome ".$name." to the Killa Server Network.");
 
-	public function onDisable(){
-		$this->getLogger()->info(TextFormat::DARK_RED . "I've been disabled!");
-	}
-
-	public function onCommand(CommandSender $sender, Command $command, $label, array $args){
-		switch($command->getName()){
-			case "example":
-				$sender->sendMessage("Hello ".$sender->getName()."!");
-				return true;
-			default:
-				return false;
-		}
-	}
-
-	/**
-	 * @param PlayerRespawnEvent $event
-	 *
-	 * @priority        NORMAL
-	 * @ignoreCancelled false
-	 */
-	public function onSpawn(PlayerRespawnEvent $event){
-		Server::getInstance()->broadcastMessage($event->getPlayer()->getDisplayName() . " has just spawned!");
-	}
+       
+    }
+   
 }
